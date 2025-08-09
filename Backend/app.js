bdf4f3d0c8dcbd55153faea789c258e50589f406
uditkumar08 <udit2162@gmail.com>
2025-08-09 23:07:50 +0530

    one

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,13 @@ const cors = require("cors")
 
 
 const app  = express()
-
+const authRoutes = require('./routes/authRoutes.js');
 
 app.use(cors())
 app.use(express.json())
 
+
+app.use('/api/auth',authRoutes)
 
 app.get("/",(req,res) => {
     res.json({
